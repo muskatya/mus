e9d5f7ee265faa091397e54b60c4087c85aa10e8
Muskat <im.muskat@gmail.com>
2026-09-16 14:10:59 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,44 @@
-mod errors;
-mod lexer;
-mod parser;
-mod codegen;
+mod cli;
+mod frontend;
+mod backend;
 
+use cli::cli::Cli;
 use clap::Parser;
 use colored::Colorize;
 use inkwell::context::Context;
-use std::process;
 use std::fs;
-
-#[derive(Parser)]
-#[command(name = "mus", version = "0.2.0", about = "Mus programming language")]
-enum Cli {
-    /// Run Mus program using JIT compilation
-    Run,
-    /// Compile Mus program to an executable
-    Build
-}
+use std::path::Path;
+use std::process;
 
 fn main() {
-    let cli = Cli::parse();
-    let source = match fs::read_to_string("main.mus") {
-        Ok(s) => s,
-        Err(e) => {
-            eprintln!("{}: {}", "error".red(), e);
-            process::exit(1);
+    let parsed = Cli::parse();
+    match parsed {
+        Cli::Run { .. } | Cli::Build { .. } => {
+            let path = Path::new("main.mus");
+            if !path.exists() {
+                eprintln!("{}: couldn't find 'main.mus'", "error".red().bold());
+                process::exit(1);
+            }
+            let source = match fs::read_to_string("main.mus") {
+                Ok(s) => s,
+                Err(e) => {
+                    eprintln!("{}: {}", "error".red().bold(), e.to_string());
+                    process::exit(1);
+                }
+            };
+            let context = Context::create();
+            let mut driver = cli::driver::Driver::new(&context);
+            match driver.compile(&source, "main.mus") {
+                Ok(_) => (),
+                Err(e) => {
+                    e.display();
+                    process::exit(1);
+                }
+            }
+            match parsed {
+                Cli::Run { release } => driver.run(release),
+                Cli::Build { release } => driver.gen_exe(release),
+            }
         }
-    };
-    let mut lex = lexer::Lexer::new(source);
-    if let Err(e) = lex.tokenize() {
-        eprintln!("{}: {}", "error".red(), e);
-        process::exit(1);
-    }
-    let mut parse = parser::Parser::new(lex.tokens);
-    let program = match parse.parse_program() {
-        Ok(p) => p,
-        Err(e) => {
-            eprintln!("{}: {}", "error".red(), e);
-            process::exit(1);
-        }
-    };
-    let context = Context::create();
-    let mut codegenerator = codegen::Codegen::new(&context);
-    if let Err(e) = codegenerator.compile_program(program) {
-        eprintln!("{}: {}", "error".red(), e);
-        process::exit(1);
-    }
-    let result = match cli {
-        Cli::Run => codegenerator.run_jit(),
-        Cli::Build => codegenerator.compile_to_executable()
-    };
-    if let Err(e) = result {
-        eprintln!("{}: {}", "error".red(), e);
-        process::exit(1);
     }
 }
